954e67e64d4019b55a6cd541d25df33b1729e873
zhangwei1 <zhangwei1@huan.tv>
2017-11-21 13:44:56 +0800

    ad...

--- kubernetes/.k8s/k8s.py
+++ kubernetes/.k8s/k8s.py
@@ -40,9 +40,9 @@ def config(setting):
     filename = setting.get('filename')
     config_map = setting.get('config_map')
     file = path + '/' + filename
-    backup_file = '.backup/' + file
-    system('mkdir -p .backup/%s' % path)
-    system('cp %s .backup/%s' % (file, path))
+    backup_file = '/vagrant/.k8s/.backup/' + file
+    system('mkdir -p /vagrant/.k8s/.backup/%s' % path)
+    system('cp %s /vagrant/.k8s/.backup/%s' % (file, path))
     with open(backup_file) as fin, open(file, 'w') as conf:
         for ln in fin:
             match_key = set()
